c3472944664832256b2d37b1cadfd34af2870e24
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2012-11-21 15:23:51 -0800

    Finished tutorial

--- src/com/example/waypoint/MainActivity.java
+++ src/com/example/waypoint/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.waypoint;
 
+import java.util.List;
+
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
 
 public class MainActivity extends MapActivity {
 
@@ -16,6 +22,15 @@ public class MainActivity extends MapActivity {
         MapView mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
 
+        List<Overlay> mapOverlays = mapView.getOverlays();
+        Drawable drawable = this.getResources().getDrawable(R.drawable.androidmarker);
+        WaypointItemizedOverlay itemizedoverlay = new WaypointItemizedOverlay(drawable, this);
+        
+        GeoPoint point = new GeoPoint(19240000,-99120000);
+        OverlayItem overlayitem = new OverlayItem(point, "Hola, Mundo!", "I'm in Mexico City!");
+     
+        itemizedoverlay.addOverlay(overlayitem);
+        mapOverlays.add(itemizedoverlay);
     }
 
  
--- src/com/example/waypoint/WaypointItemizedOverlay.java
+++ src/com/example/waypoint/WaypointItemizedOverlay.java
@@ -2,20 +2,37 @@ package com.example.waypoint;
 
 import java.util.ArrayList;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.graphics.drawable.Drawable;
 
 import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.OverlayItem;
 
 public class WaypointItemizedOverlay extends ItemizedOverlay {
-
+	Context mContext;
+	
 	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
 	
 	public WaypointItemizedOverlay(Drawable defaultMarker) {
 		super(boundCenterBottom(defaultMarker));
 		// TODO Auto-generated constructor stub
 	}
+	
+	public WaypointItemizedOverlay(Drawable defaultMarker, Context context) {
+		  super(boundCenterBottom(defaultMarker));
+		  mContext = context;
+	}
 
+	@Override
+	protected boolean onTap(int index) {
+	  OverlayItem item = mOverlays.get(index);
+	  AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+	  dialog.setTitle(item.getTitle());
+	  dialog.setMessage(item.getSnippet());
+	  dialog.show();
+	  return true;
+	}
 
 	@Override
 	protected OverlayItem createItem(int i) {
@@ -26,6 +43,8 @@ public class WaypointItemizedOverlay extends ItemizedOverlay {
 	public int size() {
 	  return mOverlays.size();
 	}
+	
+	
 	
 	public void addOverlay(OverlayItem overlay) {
 	    mOverlays.add(overlay);
